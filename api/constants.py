8d876e15eb5850f92a3c492c5aaad01499d681a6
cristianaquino <ycristian10@gmail.com>
2024-02-27 15:49:11 -0500

    feat: const for routes for api

---
+++ api/constants.py
@@ -0,0 +1,14 @@
+class Routers:
+    TEACHER = 'teacher/'
+    STUDENT = 'student/'
+    COURSE = 'course/'
+    LEVEL = 'level/'
+    SECTION = 'section/'
+    NOTE = 'note/'
+    DEGRE = 'degree/'
+    ATTENDANCE = 'attendance/'
+
+
+class NinjaApi:
+    BASE_URL = 'api/'
+    VERSION = 'v1/'
