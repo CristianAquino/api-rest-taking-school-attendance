1744921a49800a9610aeaaa55e4b3bdd7b72aa77
cristianaquino <ycristian10@gmail.com>
2024-02-29 10:52:41 -0500

    feat: initial teacher model

---
+++ teacher/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+import uuid
+
+# modificamos el user por defecto
+# de acorde a nuestro modelo
+from django.contrib.auth.models import AbstractBaseUser
+from core.models import TimeStampedModel
+from .managers import CustomUserManager
+
+# Create your models here.
+
+
+class User(AbstractBaseUser, TimeStampedModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True, error_messages={
+                              'unique': 'Email already exists.'})
+    name = models.CharField(max_length=240)
+    first_name = models.CharField(max_length=80)
+    second_name = models.CharField(max_length=80)
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
