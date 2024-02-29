3cf61be83ec418665896c8bad62323f2d0c145b9
cristianaquino <ycristian10@gmail.com>
2024-02-29 10:53:01 -0500

    feat: const value for rute

---
+++ teacher/constants.py
@@ -0,0 +1,7 @@
+class Endpoints:
+    GET_ME = 'me/'
+    GET_RANDOM_USER = 'random/'
+    PATCH_MY_ACCOUNT = 'me/'
+    POST_ADD_USER = 'add/'
+    POST_LOGIN = 'login/'
+    POST_LOGOUT = 'logout/'
