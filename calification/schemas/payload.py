6bd065762e6ed64c6cfec498d7e46ad61d10152b
cristianaquino <ycristian10@gmail.com>
2024-03-02 18:59:16 -0500

    feat: payload for update calification

--- calification/schemas/payload.py
+++ calification/schemas/payload.py
@@ -6,3 +6,8 @@ from ninja import Schema
 class PayloadPostAddCalification(Schema):
     id: UUID
     califications: List[float]
+
+
+class PayloadUpdateStudentCalification(Schema):
+    id: UUID
+    calification: int
