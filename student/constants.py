5579430069c7cc6464f49303f5888a7d741ba304
cristianaquino <ycristian10@gmail.com>
2024-03-01 21:26:32 -0500

    feat: constants of route endpoint

---
+++ student/constants.py
@@ -0,0 +1,6 @@
+class Endpoints:
+    GET_STUDENT = 'one/{id}'
+    GET_STUDENT_LIST = 'list/'
+    PUT_STUDENT = 'update/{id}'
+    POST_ADD_STUDENT = 'create/{course_id}'
+    DELETE_STUDENT = 'delete/{id}'
