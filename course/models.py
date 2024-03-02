68ea523594de643bdb7c09a51586431f43695e84
cristianaquino <ycristian10@gmail.com>
2024-03-02 11:59:59 -0500

    feat: migation

--- course/models.py
+++ course/models.py
@@ -10,9 +10,9 @@ from teacher.models import User
 class Course(TimeStampedModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=80)
-    level = models.CharField(max_length=80, default="primary")
-    degree = models.CharField(max_length=1, default="1")
-    section = models.CharField(max_length=80, default="A")
+    level = models.CharField(max_length=80)
+    degree = models.CharField(max_length=1)
+    section = models.CharField(max_length=80)
 
     # foreingkeys
     teacher = models.ForeignKey(
